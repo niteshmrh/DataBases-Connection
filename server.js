7cad0ef699763c2cf27d48d990b3aaf29a8ab943
niteshmrh <nitesh.mca20.du@gmail.com>
2023-02-22 10:26:39 +0530

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const color = require("cli-color");
 const mysql = require("mysql");
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3500, () => {
   console.log(color.white("Server Connected!!!"));
@@ -17,16 +23,93 @@ const dbconnect = mysql.createConnection({
 
 dbconnect.connect(() => console.log(color.white("Database Connected")));
 
+// ******************** Testing the home page that :- it gives output or not
 app.get("/", (req, res, next) => {
   res.send("Testing............................");
 });
 
+//*********************** getting all the data from  the table ***************************
+
 app.get("/api/person", (req, res, next) => {
   dbconnect.query("select * from person", (error, result) => {
+    if (error) {
+      return res.send({
+        error: true,
+        message: error.sqlMessage,
+        data: null,
+      });
+    }
     res.status(200).json({
       status: true,
       message: "User fetched successfully!!",
       data: result,
     });
   });
+});
+
+//***************  getting particular data from the sql ************************
+
+app.get("/api/person/:id", (req, res, next) => {
+  const id = req.params.id;
+  if (!id) {
+    return res.status(400).json({
+      error: true,
+      message: "Please provide user id",
+      data: null,
+    });
+  }
+  dbconnect.query(
+    "select * from person where personId =  ?",
+    [id],
+    (error, result) => {
+      if (error) {
+        return res.status(403).json({
+          error: true,
+          message: error.sqlMessage,
+          data: null,
+        });
+      }
+      console.log(result);
+
+      res.status(200).json({
+        status: true,
+        message: "User fetched successfully!!",
+        data: result,
+      });
+    }
+  );
+});
+
+//******************** inserting data into database person*****************************/
+
+app.post("/api/person", (req, res) => {
+  const person = req.body;
+
+  if (!person) {
+    return res.status(400).json({
+      error: true,
+      message: "Please provide person",
+      data: null,
+    });
+  }
+  // ${person.personId} personId,
+  // (Name, Email, Pan, Mobile, Aadhar) VALUES('${person.Name}', '${person.Email}', '${person.Pan}', '${person.Mobile}', '${person.Aadhar}')`,
+  dbconnect.query(
+    "INSERT INTO person SET ?",
+    person,
+    (error, results, fields) => {
+      if (error) {
+        return res.status(403).json({
+          error: true,
+          message: error.sqlMessage,
+          data: null,
+        });
+      }
+      return res.status(201).json({
+        error: false,
+        message: "User create successfully!!",
+        data: `Person Id ${results.insertId} inserted`,
+      });
+    }
+  );
 });
